460240673c1af77edc572ccb2b2b64b22690e76c
Pedro Costa <pedroclc@gmail.com>
2022-11-30 17:45:02 -0300

    WDFT W7D3 Lab Recipes 30/11/2022

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
-const mongoose = require('mongoose');
+import * as dotenv from "dotenv";
+import dbConnect from "./config/db.config.js";
+import Recipe from "./models/Recipe.model.js";
+import data from "./data.json" assert { type: "json" };
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// Import of the data from './data.json'
+//const data = require("./data");
+
+dotenv.config();
+
+dbConnect()
+  .then(() => Recipe.deleteMany())
+  .then(() =>
+    Recipe.create({
+      title: "bolo",
+      level: "Easy Peasy",
+      ingredients: "a, b, c",
+      cuisine: "bla",
+      dishType: "dessert",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 40,
+      creator: "unknown",
+      created: "11/30/2022",
+    })
+  );
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+// Before adding any recipes to the database, let's remove all existing ones
+//const delMany = Recipe.deleteMany();
+//console.log(delMany);
+
+// const newRecipe = await Recipe.create({
+//   title: "bolo",
+//   level: "Easy Peasy",
+//   ingredients: "a, b, c",
+//   cuisine: "bla",
+//   dishType: "dessert",
+//   image: "https://images.media-allrecipes.com/images/75131.jpg",
+//   duration: 40,
+//   creator: "unknown",
+//   created: "11/30/2022",
+// });
+
+const recipes = Recipe.find();
+//console.log(typeof recipes);
+
+// Recipe.find().then((recipes) => {
+//   console.log(recipes[0].title);
+// });
+
+// try {
+//   Recipe.insertMany(data);
+//   console.log("sucesso");
+// } catch (error) {
+//   console.log("Erro ao tentar usar insertMany");
+// }
